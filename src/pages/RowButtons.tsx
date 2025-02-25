51a7eb58656793b083eb381f163f2eab019a7147
Brinda Muraleedharan <brinda.salesforce@gmail.com>
2025-02-25 15:19:12 -0500

    added empty array as default

--- src/pages/RowButtons.tsx
+++ src/pages/RowButtons.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import NumberButton from './NumberButton'
 
-function RowButtons({rowValues,fn}:{rowValues:string[],fn:(val:string)=>void}) {
+function RowButtons({rowValues=[],fn}:{rowValues:string[],fn:(val:string)=>void}) {
     const getDisplayValue=()=>rowValues.map(item=> <NumberButton key={item} displayNum={item} fn={fn}/>)
   return (
     <tr>
